d77371814f5c0373968bd61890d432bb373cd96b
GitHub <31639057+1006008051@users.noreply.github.com>
2022-10-16 01:00:22 +0800

    删除script中无用文件

--- script/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.vue' {
-    const component: import("vue").DefineComponent<{}, {}, any>
-    export default component
-}
-
-declare module 'vitepress-demoblock' {
-    const demoblock: (md: any) => void;
-    export default demoblock
-}
